8eecd5594e4c4b784e2709f12f856a8dec351608
kazoottt <yjwang.echo@gmail.com>
2023-09-12 17:53:56 +0800

    feat:相交链表

---
+++ 160.相交链表.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=typescript
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+    if (!headA || !headB) return null;
+    let pA = headA;
+    let pB = headB;
+    while (pA !== pB) {
+        pA = pA === null ? headB : pA.next;
+        pB = pB === null ? headA : pB.next;
+    }
+    return pA;
+};
+// @lc code=end
+
